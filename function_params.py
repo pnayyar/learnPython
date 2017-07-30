a6ad5ae7ea3170c67714e21f07808a02f608b532
prachi <prachinayyar@gmail.com>
2017-07-30 11:35:25 +0530

    function params corrected

--- function_params.py
+++ function_params.py
@@ -1,6 +1,6 @@
-def print_msg(msg, error="No Error", *kwargs):
+def print_msg(msg, error="No Error", *args, **kwargs):
     print("Log : " +error+ " :" + msg)
-    print(kwargs)
+    print(args, kwargs)
 
-print_msg("This will be logged", "File not found", "1","2","3","4","5") # Kwrgs consider it as dic so if I give error="File not found", it will throw errroo as it is a
+print_msg("This will be logged", "File not found", "a","b","c", key_1="1", key_2="5")# Kwrgs consider it as dic so if I give error="File not found", it will throw errroo as it is a
 
