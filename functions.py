5016459a31e341b2ce8d202f5733d60671aac49c
prachi <prachinayyar@gmail.com>
2017-07-30 10:53:34 +0530

    Functions example

---
+++ functions.py
@@ -0,0 +1,6 @@
+#!//user/bin/python3
+
+def print_this():
+    print ("This is called in a function")
+print ("Outside print_this function")
+print_this()
---
+++ user_input.py
@@ -0,0 +1,8 @@
+print("Enter a name")
+s = input()
+print(s)
+
+
+print("Enter a number : ")
+s = input()
+print(s)
